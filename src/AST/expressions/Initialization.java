89df737276b15ad93af11557cf962b6016493e5d
swipeeh <merryem73@gmail.com>
2021-10-05 21:34:37 +0200

    first attempts on ASTs

---
+++ src/AST/expressions/Initialization.java
@@ -0,0 +1,4 @@
+package AST.expressions;
+
+public class Initialization {
+}
---
+++ src/AST/expressions/Integer.java
@@ -0,0 +1,13 @@
+package AST.expressions;
+
+public class Integer extends Initialization {
+    private String identity;
+
+    public intInitialization(String iD){
+        this.identity = iD;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+}

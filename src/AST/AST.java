8e3124bcc78195380d5c62f6aceac9512b1d9743
Silas Stryhn <silas_es29@hotmail.com>
2021-10-05 23:01:13 +0200

    Added more methods

--- src/AST/AST.java
+++ src/AST/AST.java
@@ -2,18 +2,48 @@ package AST;
 
 import java.util.ArrayList;
 
+
+
+
 public class AST {
 
-    private int value;
+    private int nodeData;
     private AST leftNode;
     private AST rightNode;
     private ArrayList<AST> children = new ArrayList<>();
     private AST parent = null;
 
 
-    public AST(int value) {
-        System.out.println("Test");
+    public AST(int nodeData) {
+        this.nodeData = nodeData;
     }
+
+    // Adds child to parent node and adds it to the list of children
+    public AST addChild(AST child) {
+         child.setParent(this);
+         this.children.add(child);
+         return child;
+    }
+
+    private void setParent(AST parent) {
+        this.parent = parent;
+    }
+
+    // Returns parent of the current node from which it is called
+    private AST getParent() {
+        return parent;
+    }
+
+    // Returns value of the node from which it is called
+    public int getNodeData() {
+        return nodeData;
+    }
+
+    // Sets value of node
+    public void setNodeData(int data) {
+        this.nodeData = data;
+    }
+
 
 
 

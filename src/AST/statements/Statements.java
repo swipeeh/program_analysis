71dc141468da5edd5fa04115637a11efc08d7072
Silas Stryhn <silas_es29@hotmail.com>
2021-10-25 22:10:54 +0200

    Implements Tracer interface and its methods

--- src/AST/statements/Statements.java
+++ src/AST/statements/Statements.java
@@ -1,6 +1,11 @@
 package AST.statements;
 
-public class Statements {
+import AST.Tracer;
+import AST.expressions.arrayExpressions.aExpressions;
+import AST.expressions.booleanExpressions.bExpressions;
+import AST.expressions.lexpressions.lExpressions;
+
+public class Statements implements Tracer {
     public Statements statementOne() {
         return null;
     }
@@ -9,5 +14,33 @@ public class Statements {
         return null;
     }
 
+    public bExpressions booleanExpression() {
+        return null;
+    }
+
+    @Override
+    public String getidentifier() {
+        return null;
+    }
+
+    @Override
+    public aExpressions getAExpression1() {
+        return null;
+    }
+
+    @Override
+    public aExpressions getAExpression2() {
+        return null;
+    }
+
+    @Override
+    public lExpressions getLExpression() {
+        return null;
+    }
+
+    @Override
+    public Integer getSize() {
+        return null;
+    }
     //implementation for boolean expressions;
 }

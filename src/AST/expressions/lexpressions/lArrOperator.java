6ce8ffe27c52d97b6fb813b67d2f8bacb46dc67f
Silas Stryhn <silas_es29@hotmail.com>
2021-10-25 22:10:17 +0200

    Added constructor

--- src/AST/expressions/lexpressions/lArrOperator.java
+++ src/AST/expressions/lexpressions/lArrOperator.java
@@ -3,6 +3,11 @@ package AST.expressions.lexpressions;
 public class lArrOperator extends lExpressions{
     protected String identifier;
 
+
+    public lArrOperator() {
+
+    }
+
     @Override
     public String getIdentifier(){
         return this.identifier;

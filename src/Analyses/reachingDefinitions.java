c6780a6dd4a2c8164f2be3ccfe704fa38aa82ebc
swipeeh <Merryem73@gmail.com>
2021-11-29 12:55:29 +0100

    Method for Kill -ing Nodes in reaching definitions class

--- src/Analyses/reachingDefinitions.java
+++ src/Analyses/reachingDefinitions.java
@@ -18,6 +18,7 @@ public class reachingDefinitions implements Analysis{
         initialize();
     }
 
+    //This method is being used to create all necessary lists and variables which will be used in new analysis set.
     public void initialize(){
         this.analysisSet = new HashMap<>();
         this.infl = new HashMap<>();
@@ -33,17 +34,20 @@ public class reachingDefinitions implements Analysis{
         for (Nodes n : ControlFlowAnalysis.nodesList){
             for(Edges e : ControlFlowAnalysis.edgesList){
                 if(n.equals(e.getBeginNode())){this.infl.get(n.getId()).add(e.getEndNode().getId());}}}}
-    
+
+    //return the whole analysisSet
     public HashMap<Integer, ArrayList<ArrayList<String>>> getAnalysisSet() {
        return this.analysisSet;
     }
-    
+
+    //inserting updated nodes into analysis set
     public void updateAnalysisSet(Integer l, ArrayList<ArrayList<String>> nSolution) {
         for(ArrayList<String> aList : nSolution){
             if(!this.analysisSet.get(l).contains(aList)){this.analysisSet.get(l).add(aList);}}}
     
     public HashMap<Integer, ArrayList<Integer>> getInfl() {return this.infl;}
 
+    //method which creates the constraint
     public ArrayList<ArrayList<String>> newConstraint(Integer l) {
         ArrayList<ArrayList<String>> r = new ArrayList<>();
         if(l!=0){
@@ -56,6 +60,43 @@ public class reachingDefinitions implements Analysis{
             }
         }
         return r;
+    }
+
+    //Method for removing Node from set.
+    public ArrayList<ArrayList<String>> kNode(Edges e, ArrayList<ArrayList<String>> reachingDefinitionsElement){
+        ArrayList<ArrayList<String>> r = new ArrayList<>();
+        if(e.getTracer().getClass().getName().equals("AST.declaration.IntegerDec") || e.getTracer().getClass().getName().equals("AST.declaration.ArrayDec")) {
+            ArrayList<String> t = new ArrayList<>();
+            t.add(e.getTracer().getidentifier());
+            t.add("0");
+            r.add(t);
+        }
+        else if(e.getTracer().getClass().getName().equals("AST.declaration.RecordDec")){
+            ArrayList<String> t = new ArrayList<>();
+            t.add(e.getTracer().getidentifier() +"FST");
+            t.add("0");
+            r.add(t);
+            ArrayList<String> t2 = new ArrayList<>();
+            t2.add(e.getTracer().getidentifier() +"SND");
+            t2.add("0");
+            r.add(t2);
+        }
+        else if (e.getTracer().getClass().getName().equals("AST.statements.RecordAssignment")){
+            for(ArrayList<String> sArray : reachingDefinitionsElement){
+                for(String s : sArray){
+                    if(s.equals(e.getTracer().getidentifier()+"FST")){r.add(sArray);}
+                    else if(s.equals(e.getTracer().getidentifier()+"SND")){r.add(sArray);}}}}
+        return r;
+    }
+
+    public ArrayList<ArrayList<String>> gNode(Edges e){
+        ArrayList<ArrayList<String>> r = new ArrayList<>();
+        if(e.getTracer().getClass().getName().equals("AST.declaration.IntegerDec")){
+        ArrayList<String> t = new ArrayList<>();
+        t.add(e.getTracer().getidentifier() +"FST");
+        t.add(e.getBeginNode().getId().toString());
+        t.add(e.getEndNode().getId().toString());
+        }
     }
 
     @Override
